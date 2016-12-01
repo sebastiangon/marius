73f7fb372cd2d7791db78755db43e55bc01b6c85
Sebastian Gonzalez <sebastiangon11@gmail.com>
2016-11-30 21:39:54 -0300

    first box creted with physics

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,17 @@ var Box = require('./mystery_box');
 var game = new Phaser.Game(800,600,Phaser.AUTO,'',{preload:preload, create:create, update:update});
 
 function preload(){
+  game.load.image('mystery_box','../assets/mystery_box.png');
 }
 
 function create(){
-  var b1 = Box.box(1);
-  b1.sayId();
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+    boxes = game.add.group();
+    boxes.enableBody = true;
+    var box_1 = Box.createBox(boxes,50,100);
+
 }
 
 function update(){
--- public/js/mystery_box.js
+++ public/js/mystery_box.js
@@ -1,17 +1,16 @@
 module.exports = {
-  box:box
+  createBox:createBox
 }
 
-function box(id){
+function createBox(group,x,y){
 
-  var _id= id;
-
-  function sayId(){
-    console.log(_id);
-  }
-
+  var _box = group.create(x,y,'mystery_box');
+  _box.body.immovable = true;
+  _box.body.gravity.y = 50;
+  _box.body.bounce.y = 0.8;
+  _box.body.collideWorldBounds = true;
   return {
-    sayId : sayId
+    box : _box
   }
 
 }
